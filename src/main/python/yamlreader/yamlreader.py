baaccffe9b72936db5f9b776d118b0632c999b9f
Erwin Müller <erwin.mueller@nttdata.com>
2017-05-29 12:15:12 +0200

    Change to dump

--- src/main/python/yamlreader/yamlreader.py
+++ src/main/python/yamlreader/yamlreader.py
@@ -2,7 +2,7 @@ from __future__ import print_function, absolute_import, unicode_literals, divisi
 
 __version__ = '3.0.3'
 
-from yaml import MarkedYAMLError, safe_load, safe_dump
+from yaml import MarkedYAMLError, safe_load, safe_dump, dump
 import glob
 import os
 import logging
@@ -136,7 +136,7 @@ def __main():
     if not args:
         parser.error("Need at least one argument")
     try:
-        print(safe_dump(yaml_load(args, defaultdata={}),
+        print(dump(yaml_load(args, defaultdata={}),
                         indent=4, default_flow_style=False, canonical=False))
     except Exception as e:
         parser.error(e)
